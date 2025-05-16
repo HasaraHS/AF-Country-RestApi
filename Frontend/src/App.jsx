2c02d26fa3ab243a2dadc51a1bef8e45ceda2f5a
IT22342058 <IT22342058@my.sliit.lk>
2025-05-16 08:24:51 +0530

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WorldGlobe from "./Components/WorldGlobe";
 import Home from "./Pages/Home";
-import Test2 from "./Pages/Test2";
 import Login from "./Pages/Login";
 
 
@@ -10,7 +8,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/globe" element={<WorldGlobe />} />
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
